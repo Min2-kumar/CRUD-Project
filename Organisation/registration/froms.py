3ed42f0a54fc3f69fef96c6d94941be3e50779ea
Min2-Kumar <mkreborn2@gmail.com>
2023-12-14 19:57:53 +0530

    add model, created admin pannel

---
+++ Organisation/registration/froms.py
@@ -0,0 +1,4 @@
+from django import forms
+from registration.models import User
+
+class EmployeeRegistration(forms.ModelForm):
--- Organisation/registration/models.py
+++ Organisation/registration/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField('Name',max_length=70)
+    email = models.EmailField('Email',max_length=70)
+    password = models.CharField('Password',max_length=70)
